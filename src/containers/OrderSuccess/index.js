361faabaad9a9c6a0524fa1d971fcebce92773f1
Amine <amine@anava.tn>
2020-07-01 06:02:57 +0100

    fix: add another button to redirect to orders after submission

--- src/containers/OrderSuccess/index.js
+++ src/containers/OrderSuccess/index.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import { Result, Button } from 'antd';
+import React from "react";
+import { Result, Button } from "antd";
+import { connect } from "react-redux";
 
-const Success = ({ history }) => {
+const Success = ({ history, user }) => {
   return (
     <Result
       status="success"
       title="Order registered"
       subTitle="Your order has been registered. We won't let you wait for too long."
       extra={
-        <Button type="primary" onClick={() => history.push('/')}>
-          Back Home
-        </Button>
+        <>
+          <Button type="primary" onClick={() => history.push("/")}>
+            Back Home
+          </Button>
+          {user && (
+            <Button type="info" onClick={() => history.push("/orders")}>
+              Check orders
+            </Button>
+          )}
+        </>
       }
     />
   );
 };
 
-export default Success;
+const mapStateToProps = ({ user }) => ({
+  user,
+});
+
+export default connect(mapStateToProps, null)(Success);
